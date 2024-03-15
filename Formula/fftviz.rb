266399d7472b7832be224101a89e3ac515a7f404
gursi26 <gursimar.batra@gmail.com>
2024-03-15 16:26:28 -0400

    update version

--- Formula/fftviz.rb
+++ Formula/fftviz.rb
@@ -1,9 +1,9 @@
 class Fftviz < Formula
   desc "A lightweight, customizable FFT visualizer for audio files"
   homepage "https://github.com/gursi26/fftviz"
-  url "https://github.com/gursi26/fftviz/releases/download/v0.3.0/fftviz-mac.tar.gz"
-  sha256 "35124c7ebc9d3647e97fde6c5888b0746dbd25b60fb2b882a9e16d87c33eebf6"
-  version "0.3.0"
+  url "https://github.com/gursi26/fftviz/releases/download/v0.3.1/fftviz-mac.tar.gz"
+  sha256 "e515b28e7c2c3188ca64b9e8d10c5744be2d75c33f2521676d1892a5ad90dd69"
+  version "0.3.1"
 
   def install
     bin.install "fftviz"
